5e36430423aaf172b396179f728feb44cbb0e137
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-06-07 18:25:17 -0300

    feat: add images and social links constants

---
+++ src/constants/images.ts
@@ -0,0 +1,5 @@
+const images: Record<string, string> = {
+  logo: "/src/public/img/logo.png",
+};
+
+export default images;
---
+++ src/constants/social-links.ts
@@ -0,0 +1,7 @@
+export const socialLinks: Map<string, string> = new Map([
+  ["instagram", "https://www.instagram.com/restaurantefredosa/#"],
+  [
+    "whatsapp",
+    "https://api.whatsapp.com/send/?phone=5511954884470&text&type=phone_number&app_absent=0",
+  ],
+]);
